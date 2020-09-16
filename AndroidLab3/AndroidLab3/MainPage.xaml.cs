86761b3d0d099c80fafe03ee3dc887b6a15c6819
Мартыновы Мартыновы <danilmartynov71@gmail.com>
2020-09-16 09:45:07 +0300

    LB4 android

---
+++ AndroidLab3/AndroidLab3/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidLab3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        bool alive = false;
+        private DateTime start;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (alive == true)
+            {
+                alive = false;
+            }
+            else
+            {
+                alive = true;
+                start = DateTime.Now;
+                Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+
+            }
+        }
+        private bool OnTimerTick()
+        {
+            timerLabel.Text = (DateTime.Now-start).ToString("T");
+            return alive;
+        }
+    }
+}
